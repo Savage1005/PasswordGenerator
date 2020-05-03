a60c76f523a1e998d91f7936d799a845a78cb4c4
Tom Savage <tsavage@gmail.com>
2020-05-02 23:25:31 -0400

    found missing semi-colons

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var specials= [ "!", "#", "$", "%", "&", "(", ")", "*", "+", "/", ":", ";", "<", "=", ">", "?"]
-var numbers= [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
-var uppers = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P","Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowers = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var specials= [ "!", "#", "$", "%", "&", "(", ")", "*", "+", "/", ":", ";", "<", "=", ">", "?"];
+var numbers= [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ];
+var uppers = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P","Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowers = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Write password to the #password input
 function writePassword() {
@@ -80,7 +80,7 @@ for (var i = 0; i <passwordLength; i++){
   possibles[Math.floor(Math.random() * possibles.length)];
   var randomChar=
   randomArray[Math.floor(Math.random() * randomArray.length)];
-  result += randomChar
+  result += randomChar;
 }
 console.log(result);
 return result;
